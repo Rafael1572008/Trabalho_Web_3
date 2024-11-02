5a0e847ef4f18546f920e5f63334ecc2f34a6dcc
Rafael Pires Gonçalves <127984148+Rafael1572008@users.noreply.github.com>
2024-11-02 16:25:09 -0300

    reservas

--- src/hospede/js/reservas.js
+++ src/hospede/js/reservas.js
@@ -1,16 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // obter formulário e sua lista
   const formReserva = document.getElementById("form-reserva");
   const listaReservas = document.getElementById("lista-reservas");
 
-  // obter a lista anterior
   const reservas = JSON.parse(localStorage.getItem("reservas")) || [];
-
   atualizarListaReservas();
 
-  // preços dos quartos
   const preco = {
-    "Duplo Solteiro": 150,
+    "Duplo Solteiro": 120,
     "Quarto Casal": 200,
     Dormitórios: 100,
     Apartamentos: 250,
@@ -19,47 +15,37 @@ document.addEventListener("DOMContentLoaded", function () {
     "Deluxe ou Master Superior": 400,
   };
 
-  // preços dos serviços extras
   const precoServicosExtras = {
     Lavanderia: 50,
     Massagem: 120,
     Restaurante: 70,
   };
 
-  // impede o carregamento da página ao enviar o formulário
   formReserva.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    // captura dos elementos
     const nomeHospede = formReserva.elements[0].value;
     const telefoneHospede = formReserva.elements[1].value;
     const tipoQuarto = formReserva.elements[2].value;
     const checkIn = formReserva.elements[3].value;
     const checkOut = formReserva.elements[4].value;
 
-    // captura dos serviços extras selecionados
     const servicosExtrasSelecionados = Array.from(
       formReserva.querySelectorAll('input[name="servico-extra"]:checked')
     ).map((checkbox) => checkbox.value);
 
-    // calcula a quantidade de dias
     const dias = Math.ceil(
       Math.abs(new Date(checkOut) - new Date(checkIn)) / (1000 * 60 * 60 * 24)
     );
 
-    // calcula o custo total do quarto
     const custoQuarto = (preco[tipoQuarto] || 0) * dias;
-
-    // calcula o custo total dos serviços extras selecionados
     const custoServicosExtras = servicosExtrasSelecionados.reduce(
       (total, servico) => total + (precoServicosExtras[servico] || 0),
       0
     );
 
-    // custo total da reserva, incluindo quarto e serviços extras
     const custoTotal = custoQuarto + custoServicosExtras;
 
-    // adiciona a reserva à lista de reservas
     reservas.push({
       nomeHospede,
       telefoneHospede,
@@ -72,12 +58,10 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     localStorage.setItem("reservas", JSON.stringify(reservas));
 
-    // atualiza a lista de reservas e reseta o formulário
     atualizarListaReservas();
     formReserva.reset();
   });
 
-  // função para atualizar a lista de reservas
   function atualizarListaReservas() {
     listaReservas.innerHTML = "";
 
@@ -85,7 +69,6 @@ document.addEventListener("DOMContentLoaded", function () {
       const div = document.createElement("div");
       div.classList.add("reserva");
 
-      // Cria elementos para cada informação e adiciona ao div
       const hospede = document.createElement("p");
       hospede.textContent = `Hóspede: ${reserva.nomeHospede}`;
       div.appendChild(hospede);
@@ -122,7 +105,6 @@ document.addEventListener("DOMContentLoaded", function () {
       custo.textContent = `Custo Total: R$${reserva.custoTotal.toFixed(2)}`;
       div.appendChild(custo);
 
-      // Adiciona a reserva à lista
       listaReservas.appendChild(div);
     });
   }
