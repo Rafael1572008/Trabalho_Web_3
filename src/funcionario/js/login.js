e0479f8553b5159bf2c1945c54a1d6219bc9957e
Rafael Pires Gonçalves <127984148+Rafael1572008@users.noreply.github.com>
2024-11-02 22:17:35 -0300

    login de ADM

--- src/funcionario/js/login.js
+++ src/funcionario/js/login.js
@@ -14,6 +14,15 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("Email digitado:", emailLogin);
     console.log("Senha digitada:", senhaLogin);
 
+    // Lista de administradores
+    const lista_Adm = [
+      { nome: "Kaio@gmail.com", password: "12345" },
+      { nome: "Rafael@gmail.com", password: "12345" },
+      { nome: "Sara@gmail.com", password: "12345" },
+      { nome: "Mateus@gmail.com", password: "12345" },
+      { nome: "Claudete@gmail.com", password: "12345" } 
+    ];
+
     // Recupera a lista de hóspedes do localStorage
     const lista_Hospedes = JSON.parse(localStorage.getItem("hospedes")) || [];
     console.log("Lista de hóspedes no localStorage:", lista_Hospedes);
@@ -23,13 +32,25 @@ document.addEventListener("DOMContentLoaded", function () {
       (hospede) => hospede.email === emailLogin && hospede.senha === senhaLogin
     );
 
+    // Verifica primeiro para hóspedes, dps verificar adm
     if (hospedeEncontrado) {
-      alert("Login realizado com sucesso!");
-      window.location.href = "../hospede/hospede.html"; // Redireciona para a página principal
+      alert("Login efetuado com sucesso");
+      window.location.href = "../hospede/hospede.html"; // Redireciona para a página do hóspede
     } else {
-      alert("Email ou senha incorretos!");
-      console.log("Email ou senha não correspondem a nenhum hóspede cadastrado.");
-      console.log(`${lista_Hospedes}`)
+      // Se não encontrar, verifica na lista de administradores
+      const adminEncontrado = lista_Adm.find(
+        (adm) => adm.nome === emailLogin && adm.password.toString() === senhaLogin
+      );
+
+      // Resultado
+      if (adminEncontrado) {
+        alert("Login efetuado com sucesso");
+        window.location.href = "../funcionario/painel.html"; // Redireciona para a página do funcionário
+      } else {
+        alert("Email ou senha incorretos!");
+        console.log("Lista de hóspedes:", lista_Hospedes);
+        console.log("Lista de administradores:", lista_Adm);
+      }
     }
   });
 });
