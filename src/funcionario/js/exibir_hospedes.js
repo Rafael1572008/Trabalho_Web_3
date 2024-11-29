50946a76070df46a8178a41aeb84a0b818a107fe
Rafael Pires Gonçalves <127984148+Rafael1572008@users.noreply.github.com>
2024-11-29 20:59:02 -0300

    Solução  visu
    Tira meu auxilío

--- src/funcionario/js/exibir_hospedes.js
+++ src/funcionario/js/exibir_hospedes.js
@@ -1,27 +1,26 @@
 // Função para exibir os hóspedes na tabela
 function exibir_Hospedes() {
-    const lista_Hospedes = document.getElementById("lista_Hospedes");
-    lista_Hospedes.innerHTML = ""; // Limpa a lista
-  
-    // Obtendo a lista de hóspedes do localStorage
-    const Hospedes = JSON.parse(localStorage.getItem("hospedes")) || [];
-  
-    // Itera sobre os hóspedes e cria linhas para cada um
-    Hospedes.forEach((hospede) => {
-      const row = document.createElement("tr");
-  
-      row.innerHTML = `
-        <td>${hospede.nome}</td>
-        <td>${hospede.cpf}</td>
-        <td>${hospede.telefone}</td>
-        <td>${hospede.email}</td>
-        <td>${hospede.endereco}</td>
-      `;
-  
-      lista_Hospedes.appendChild(row);
-    });
-  }
-  
-  // Chama a função ao carregar a página
-  window.onload = exibir_Hospedes;
-  
+  const lista_Hospedes = document.getElementById("lista_Hospedes");
+  lista_Hospedes.innerHTML = ""; // Limpa a lista
+
+  // Obtendo a lista de hóspedes do localStorage
+  const Hospedes = JSON.parse(localStorage.getItem("hospedes")) || [];
+
+  // Itera sobre os hóspedes e cria linhas para cada um
+  Hospedes.forEach((hospede) => {
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+      <td>${hospede._nome}</td>
+      <td>${hospede._cpf}</td>
+      <td>${hospede._telefone}</td>
+      <td>${hospede._email}</td>
+      <td>${hospede._endereco}</td>
+    `;
+
+    lista_Hospedes.appendChild(row);
+  });
+}
+
+// Chama a função ao carregar a página
+window.onload = exibir_Hospedes;
